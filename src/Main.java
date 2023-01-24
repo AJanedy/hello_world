788e738ecc8514eb6ea4ecc6ca39c1712326defe
andrewjanedy <andy.janedy@gmail.com>
2023-01-24 12:10:15 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * This is the Main class in a Java program
+ */
+public class Main {
+    /**
+     * This is the main method in the Java program
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println();  // println() contains newline
+        System.out.println("Hello world!");
+    }
+}
